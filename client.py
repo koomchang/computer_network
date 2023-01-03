1f0881e4ed53e57fee7a42913933b160bfaf5773
koomchang <leahpar0401@gmail.com>
2023-01-03 13:12:56 +0900

    fix: client, server 코드 정리 및 주석 추가

--- client.py
+++ client.py
@@ -1,29 +1,52 @@
 from socket import *
 
+# 서버 IP address
 serverName = '127.0.0.1'
-serverPort = 12345
 
+# 서버에서 지정한 포트번호
+serverPort = 12000
+
+# 클라이언트 소켓 객체 생성
+# AF_INET: IPv4 주소체계 사용
+# SOCK_STREAM : 소켓 타입으로 TCP 사용
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-method = {
-    'GET': 'GET / HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nUser-Agent: insomnia/2022.2.1\r\nAccept: */*\r\n\r\n',
-    'POST': 'POST / HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nUser-Agent: insomnia/2022.2.1\r\nAccept: */*\r\nContent-Length: 0\r\n\r\n',
-    'HEAD': 'HEAD / HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nUser-Agent: insomnia/2022.2.1\r\nAccept: */*\r\n\r\n',
-    'PUT': 'PUT / HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nUser-Agent: insomnia/2022.2.1\r\nAccept: */*\r\nContent-Length: 0\r\n\r\n',
-    'DELETE': 'DELETE / HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nUser-Agent: insomnia/2022.2.1\r\nAccept: */*\r\n\r\n',
-}
-while True:
-    msg = input('msg:')
 
-    # end 명령어를 치면 소켓 통신 종료
+# 지정한 Host 와 Port 로 서버에 접속
+clientSocket.connect((serverName, serverPort))
+
+# Client 측에서 요청하는 HTTP Method 와 HTTP request message
+User_Agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)'
+method = {
+    'GET': f'GET / HTTP/1.1\r\nHost: {serverName}:{serverPort}\r\nUser-Agent: {User_Agent}\r\nAccept: */*\r\n\r\n',
+    'POST': f'POST / HTTP/1.1\r\nHost: {serverName}:{serverPort}\r\nUser-Agent: {User_Agent}\r\nAccept: */*\r\nContent-Length: 0\r\n\r\n',
+    'HEAD': f'HEAD / HTTP/1.1\r\nHost: {serverName}:{serverPort}\r\nUser-Agent: {User_Agent}\r\nAccept: */*\r\n\r\n',
+    'PUT': f'PUT / HTTP/1.1\r\nHost: {serverName}:{serverPort}\r\nUser-Agent: {User_Agent}\r\nAccept: */*\r\nContent-Length: 0\r\n\r\n',
+    'DELETE': f'DELETE / HTTP/1.1\r\nHost: {serverName}:{serverPort}\r\nUser-Agent: {User_Agent}\r\nAccept: */*\r\n\r\n',
+}
+# HTTP 1.1 이므로 tcp 통신을 유지한 채 여러 데이터를 병렬식으로 보낼 수 있다.
+while True:
+    msg = input('Request Method:')
+
+    # end 를 입력하면 반복문을 빠져나간다.
     if msg == "end":
         break
+
+    # 사용자가 입력한 method 에 해당하는 request_message 를 만든다.
     if msg in method.keys():
         request_message = method[msg]
-    else:
-        request_message = 'NotAllowed / HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nUser-Agent: insomnia/2022.2.1\r\nAccept: */*\r\n\r\n'
-    clientSocket.send(request_message.encode('utf-8'))
-    recieve_message = clientSocket.recv(65535)
-    print(recieve_message.decode())
 
+    # 이외 method 를 입력하거나 잘못 입력 한 경우 'Method Not Allowed' request message 를 전달한다.
+    else:
+        request_message = f'NotAllowed / HTTP/1.1\r\nHost: {serverName}:{serverPort}\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)\r\nAccept: */*\r\n\r\n'
+
+    # 서버로 request_message 를 보낸다.
+    clientSocket.send(request_message.encode('utf-8'))
+
+    # 클라이언트 소켓으로부터 최대 1024 bytes 만큼의 데이터를 읽어 온다.
+    receive_message = clientSocket.recv(1024)
+
+    # 읽어온 데이터를 출력한다.
+    print(receive_message.decode())
+
+# 소켓 통신 종료
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,23 +1,36 @@
 import socket
-from datetime import datetime
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 서버 IP address
+serverName = '127.0.0.1'
 
-server_socket.bind(('127.0.0.1', 12345))
-server_socket.listen(0)
+# 서버에서 지정한 포트번호
+serverPort = 12000
 
-print('The server is running')
-print("Server IP : 127.0.0.1")
-print("Server Port : 12345")
-client_socket, addr = server_socket.accept()
+# 서버 소켓 객체 생성
+# AF_INET: IPv4 주소체계 사용
+# SOCK_STREAM : 소켓 타입으로 TCP 사용
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# bind : 서버가 사용 할 IP 주소와 port 번호를 결합 시키는 것
+serverSocket.bind((serverName, serverPort))
+
+# listen : client 로 부터 연결 요청이 수신이 되는지 주시 (1개의 client 만 허용)
+serverSocket.listen(1)
+
+print('Server Status : Healthy')
+print(f"Server IP : {serverName}")
+print(f"Server Port : {serverPort}\n")
+
+# accept() 는 (소켓, 주소정보)로 구성되는 튜플을 리턴한다.
+client_socket, addr = serverSocket.accept()
 
 while True:
-    data = client_socket.recv(65535)
 
+    # 클라이언트 소켓으로 부터 데이터를 읽어 온다.
+    data = client_socket.recv(1024)
     request_data = data.decode().split()
 
-    # client에서 end를 호출해 data가 넘어오지 않는 경우
+    # client에서 end를 호출해 data가 넘어오지 않는 경우 반복문을 탈출한다.
     if not data:
         break
 
@@ -27,36 +40,28 @@ while True:
 
     server_name = "Socket Server"
 
-    # 해당 method와 HTTP version이 제대로 들어올 경우 처리
-    if request_method == "GET" and request_version == "HTTP/1.1":
-        print(request_method, request_version, "200")
-        response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\n".format(request_version, server_name,
-                                                                      datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
-    elif request_method == "POST" and request_version == "HTTP/1.1":
-        print(request_method, request_version, "201")
-        response_data = "{0} 201 OK\nServer: {1}\nDate: {2}\n".format(request_version, server_name,
-                                                                      datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
-    elif request_method == "HEAD" and request_version == "HTTP/1.1":
-        print(request_method, request_version, "203")
-        response_data = "{0} 203 OK\nServer: {1}\nDate: {2}\n".format(request_version, server_name,
-                                                                      datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
-    elif request_method == "PUT" and request_version == "HTTP/1.1":
-        print(request_method, request_version, "202")
-        response_data = "{0} 202 OK\nServer: {1}\nDate: {2}\n".format(request_version, server_name,
-                                                                      datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
-    elif request_method == "DELETE" and request_version == "HTTP/1.1":
-        print(request_method, request_version, "204")
-        response_data = "{0} 204 OK\nServer: {1}\nDate: {2}\n".format(request_version, server_name,
-                                                                      datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
+    # method 에 따른 status_code 설정
+    status_code = {
+        'GET': '200',
+        'POST': '201',
+        'HEAD': '203',
+        'PUT': '202',
+        'DELETE': '204',
+    }
 
-    # 위에서 정의한 5개 이외의 method 명령어를 실행 시 405 Method Not Allowed로 응답
+    # 해당 method 와 HTTP version 이 제대로 들어올 경우 처리
+    if request_method in f"{status_code.keys()}" and request_version == "HTTP/1.1":
+        print(request_method, request_version, f"{status_code[request_method]}\n")
+        response_data = f"{request_version} {status_code[request_method]} OK\nServer: {server_name}\n"
+
+    # 이외의 method 명령어 실행 시 405 Method Not Allowed 로 response
     else:
-        print(request_method, request_version, "405 Method Not Allowed")
-        response_data = "{0} 405 Method Not Allowed\nServer: {1}\nDate: {2}\n".format(request_version, server_name,
-                                                                                      datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
+        print(request_method, request_version, "405 Method Not Allowed\n")
+        response_data = f"{request_version} 405 Method Not Allowed\nServer: {server_name}\n"
 
-
+    # 클라이언트에 response data 를 보낸다.
     client_socket.send(response_data.encode())
 
+# client 에서 data 를 넘겨주지 않아 반복문 탈출 시 소켓 통신 종료
 client_socket.close()
-server_socket.close()
+serverSocket.close()
